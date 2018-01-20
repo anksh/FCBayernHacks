8f1692b288aa7a083f8f1d278be1d9601739bac2
bystrzakk <bystry69@gmail.com>
2018-01-20 23:18:56 +0100

    Add more details anbout item and allow to take a picture

---
+++ app/src/main/java/de/hackathon/adidas/adidasproject/SustainDetailsActivity.java
@@ -0,0 +1,13 @@
+package de.hackathon.adidas.adidasproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SustainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sustain_details);
+    }
+}
--- app/src/main/java/de/hackathon/adidas/adidasproject/SustainFragment.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/SustainFragment.java
@@ -1,5 +1,6 @@
 package de.hackathon.adidas.adidasproject;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,25 +8,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SustainFragment extends Fragment {
 
+    private View view;
+    private LayoutInflater inflater;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.sustain_fragment, null);
+        this.inflater = inflater;
+        view = inflater.inflate(R.layout.sustain_fragment, null);
         ButterKnife.bind(this, view);
 
         return view;
     }
 
+    final int CAMERA_CAPTURE = 1;
 
     @OnClick(R.id.sustainCameraBtn)
     protected void openCamera() {
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
+      /*  //ONLY FOR TESTS!!!!!!!!!!!!!!!!!!!!!
+        Intent myIntent = new Intent(getActivity(), SustainDetailsActivity.class);
+        // myIntent.putExtra("key", value); //Optional parameters
+        getActivity().startActivity(myIntent);
+        //ONLY FOR TESTS!!!!!!!!!!!!!!!!!!!!!*/
+
+
+        try{
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivityForResult(intent, CAMERA_CAPTURE);
+        }catch (ActivityNotFoundException anfe){
+            String errorMessage = "your device doesn't support capturing images :(";
+            Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        Intent myIntent = new Intent(getActivity(), SustainDetailsActivity.class);
+       // myIntent.putExtra("key", value); //Optional parameters
+        getActivity().startActivity(myIntent);
+
     }
 }
+
