b877c426359fe93ab0c4535ae68589b115adea53
Ankit Shah <ank.h.shah@gmail.com>
2018-01-20 12:09:03 +0100

    Inflating fragments

--- app/src/main/java/de/hackathon/adidas/adidasproject/BuyFragment.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/BuyFragment.java
@@ -1,10 +1,26 @@
 package de.hackathon.adidas.adidasproject;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by ankit on 1/20/18.
  */
 
 public class BuyFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.buy_layout, null);
+    }
 }
