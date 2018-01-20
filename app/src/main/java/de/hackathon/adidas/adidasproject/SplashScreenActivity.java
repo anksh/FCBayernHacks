7901e24809b64964e82712a4df77950443f3eb97
bystrzakk <bystry69@gmail.com>
2018-01-20 11:31:09 +0100

    Add splashscreen

---
+++ app/src/main/java/de/hackathon/adidas/adidasproject/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package de.hackathon.adidas.adidasproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import butterknife.ButterKnife;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int splashScreenDelay = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ActivityStarter starter = new ActivityStarter();
+        starter.start();
+    }
+
+
+    private class ActivityStarter extends Thread {
+        @Override
+        public void run() {
+            try {
+                // Execute any task in background during init. BD connection etc.*
+                Thread.sleep(splashScreenDelay);
+            } catch (Exception e) {
+                Log.e("SplashScreen", e.getMessage());
+            }
+            Intent intent = new Intent(SplashScreenActivity.this, null);  // replace null with new Activity (tabbed one)
+            SplashScreenActivity.this.startActivity(intent);
+            SplashScreenActivity.this.finish();
+        }
+    }
+}
