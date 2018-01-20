bc12d911cc82ee239f7ef8b069baf253054e1429
Ankit Shah <ank.h.shah@gmail.com>
2018-01-20 12:09:03 +0100

    Inflating fragments

--- app/src/main/java/de/hackathon/adidas/adidasproject/SustainFragment.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/SustainFragment.java
@@ -1,10 +1,21 @@
 package de.hackathon.adidas.adidasproject;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by ankit on 1/20/18.
  */
 
 public class SustainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sustain_fragment, null);
+    }
 }
