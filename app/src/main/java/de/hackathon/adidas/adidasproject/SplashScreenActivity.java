4edc89b3b190923222c88a8c47a4db6430f7ce18
bystrzakk <bystry69@gmail.com>
2018-01-20 11:53:57 +0100

    Rename activity in case of merge problems

--- app/src/main/java/de/hackathon/adidas/adidasproject/SplashScreenActivity.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/SplashScreenActivity.java
@@ -15,7 +15,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashScreen);
         ButterKnife.bind(this);
 
         ActivityStarter starter = new ActivityStarter();
