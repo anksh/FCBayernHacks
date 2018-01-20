bdb1ecd8617d6678cb8dd7cc879344f54c072de1
Ovidiu01 <tuglea.ovidiu@gmail.com>
2018-01-20 16:26:37 +0100

    Display WebView

--- app/src/main/java/de/hackathon/adidas/adidasproject/BuyFragment.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/BuyFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by ankit on 1/20/18.
@@ -13,14 +16,32 @@ import android.view.ViewGroup;
 
 public class BuyFragment extends Fragment {
 
+    private WebView mWebView;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.buy_layout, null);
+        View view =  inflater.inflate(R.layout.buy_layout, container, false);
+        mWebView = (WebView) view.findViewById(R.id.buy_layout_webview);
+
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://www.adidas.com/us/men-originals-shoes");
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        return view;
+
+
     }
 }
