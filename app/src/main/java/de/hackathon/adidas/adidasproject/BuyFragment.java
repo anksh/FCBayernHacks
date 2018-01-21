5e9b2c6a62327cf41d7f8c5cc502666295e2872f
bystrzakk <bystry69@gmail.com>
2018-01-21 10:54:31 +0100

    Inform users that webpage is loading in background

--- app/src/main/java/de/hackathon/adidas/adidasproject/BuyFragment.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/BuyFragment.java
@@ -1,5 +1,6 @@
 package de.hackathon.adidas.adidasproject;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,35 +11,45 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-/**
- * Created by ankit on 1/20/18.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BuyFragment extends Fragment {
 
-    private WebView mWebView;
+    @BindView(R.id.buy_layout_webview)
+    protected WebView mWebView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.buy_layout, container, false);
-        mWebView = (WebView) view.findViewById(R.id.buy_layout_webview);
-
+        ButterKnife.bind(this, view);
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.loadUrl("http://www.adidas.com/us/men-originals-shoes");
 
         // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                mWebView.setVisibility(View.INVISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                mWebView.setVisibility(View.VISIBLE);
+                super.onPageFinished(view, url);
+            }
+        });
 
         return view;
 
