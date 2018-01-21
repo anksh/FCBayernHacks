3fb09e6c760da96845e73543ef524199a57e342a
Ankit Shah <ank.h.shah@gmail.com>
2018-01-21 11:04:38 +0100

    Floating Action Button on Lease screen

---
+++ app/src/main/java/de/hackathon/adidas/adidasproject/ScrollFAB.java
@@ -0,0 +1,54 @@
+package de.hackathon.adidas.adidasproject;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ankit on 1/21/18.
+ */
+
+public class ScrollFAB extends FloatingActionButton.Behavior {
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                        nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                /**
+                 * Called when a FloatingActionButton has been hidden
+                 *
+                 * @param fab the FloatingActionButton that was hidden.
+                 */
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+
+    public ScrollFAB(Context context, AttributeSet attrs) {
+        super();
+    }
+
+
+
+}
