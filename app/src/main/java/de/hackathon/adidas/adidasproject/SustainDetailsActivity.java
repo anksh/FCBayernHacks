2aa8e156f831c790a012e5da81b9d12fab9f07be
bystrzakk <bystry69@gmail.com>
2018-01-22 10:31:38 +0100

    snackbar

--- app/src/main/java/de/hackathon/adidas/adidasproject/SustainDetailsActivity.java
+++ app/src/main/java/de/hackathon/adidas/adidasproject/SustainDetailsActivity.java
@@ -1,15 +1,42 @@
 package de.hackathon.adidas.adidasproject;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SustainDetailsActivity extends AppCompatActivity {
+
+    @OnClick(R.id.editText43)
+    protected void showSnackbarOnClickSubmit(){
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.mainActivityPage), "Submitted! Check your email for details", Snackbar.LENGTH_SHORT)
+                .setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+        snackbar.show();
+      //  redirectToHomePage();
+    }
+
+    /*private void redirectToHomePage(){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = new LeaseFragment();
+        fm.beginTransaction().add(R.id.sus,fragment).commit();
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         setContentView(R.layout.activity_sustain_details);
+        ButterKnife.bind(this);
     }
 }
